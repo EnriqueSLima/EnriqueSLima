477649207c50cf48039a9a90f6f484839bb17ae5
EnriqueSLima <souza.enrique7@gmail.com>
2023-08-07 21:33:47 -0300

    Commenting code for better understanding

--- Univesp/c++/parameters_pointers.cpp
+++ Univesp/c++/parameters_pointers.cpp
@@ -2,45 +2,47 @@
 
 using namespace std; //to use just cout 
 
-void memory_allocated_amount(int* p)
+void memory_allocated_amount(int* p)  //  function to work with pointer p, inside function scope
 {
-  p = new int;
-  *p = 7;
+  p = new int;  //  Create a new pointer
+  *p = 7; //  Inserting a value of the new pointer
 }
-void memory_modified_value(int* p)
+void memory_modified_value(int* p)  //  function to work with pointer p, outside of function scope
 {
-  *p = 8;
+  *p = 14;  //  Changing the value without creating a new pointer, outside of function scope
 }
-void reference(int*& p)
+void reference(int*& p) //  function to work with pointer p, by reference
 {
-  p = new int;
-  *p = 9;
+  p = new int;  //  Create a new pointer with another adress because of &
+  *p = 21;  //  Inserting a value the new pointer
 }
 
 int main()
 {
   int a  = 1;
   int b  = 2;
-  int c  = 3; 
+  int c  = 3; //  Creating variable with values
   int* p1 = &a;
   int* p2 = &b;
-  int* p3 = &c;
+  int* p3 = &c; //  Creating pointers and assingn memory adress
 
-  cout << "Values: "<< a <<" "<< b <<" "<< c << endl;
-  cout << "Pointers Before: "<<  p1 <<" "<<  p2 <<" "<<  p3 << endl;
-  cout << "Values Before: "<< *p1 <<" "<< *p2 <<" "<< *p3 << endl;
+  cout << "Before:" << endl; // Before the functions call
+  cout << "Variables Values: "<< a <<" "<< b <<" "<< c << endl; //  Print variables values
+  cout << "Memory Adress: "<<  p1 <<" "<<  p2 <<" "<<  p3 << endl;  //  Print memory adress
+  cout << "Pointers Values: "<< *p1 <<" "<< *p2 <<" "<< *p3 << endl;  //  Print values inside memory adress
   cout << endl;
   
-  cout << "Modifing values with functions" << endl;
+  cout << "Modifing values with functions" << endl; // Print a message for calling functions
   cout << endl;
 
   memory_allocated_amount(p1); 
   memory_modified_value(p2);
-  reference(p3);  
+  reference(p3);  // Calling functions
 
-  cout << "Pointers After: "<<  p1 <<" "<<  p2 <<" "<<  p3 << endl;
-  cout << "Values After: "<< *p1 <<" "<< *p2 <<" "<< *p3 << endl;
-  cout << "Values: "<< a <<" "<< b <<" "<< c << endl;
+  cout << "After:" << endl; //  After functions call
+  cout << "Variables Values: "<< a <<" "<< b <<" "<< c << endl; // Print variables values
+  cout << "Memory Adress: "<<  p1 <<" "<<  p2 <<" "<<  p3 << endl;  //  Print memory adress
+  cout << "Pointers Values: "<< *p1 <<" "<< *p2 <<" "<< *p3 << endl;  //  Print values inside memory adress
 
   return 0;
 }
--- Univesp/c++/parameters_vectors.cpp
+++ Univesp/c++/parameters_vectors.cpp
@@ -1,47 +1,47 @@
 #include <iostream>
 
-void modifica_vetor_sintaxe_1(int b[], int num_elem)
+void modify_vetor_sintax_1(int b[], int num_elem)
 {
   for (int i = 0; i < num_elem; i++){
     b[i] = b[i] * 2;
   }
 }
 
-void modifica_vetor_sintaxe_2(int* b, int num_elem)
+void modify_vetor_sintax_2(int* b, int num_elem)
 {
   for (int i = 0; i < num_elem; i++){
     b[i] = b[i] * 2;
   }
 }
 
-void vetor_const_sintaxe_1(const int* b, int num_elem) 
+void vetor_const_sintax_1(const int* b, int num_elem) 
 {
-  // Corpo sem alterar os elementos do vetor
+  // Body without changing vector elements
 }
 
-void vetor_const_sintaxe_2(const int b[], int num_elem) 
+void vetor_const_sintax_2(const int b[], int num_elem) 
 {
-  // Corpo sem alterar os elementos do vetor
+  // Body without changing vector elements
 }
 
 
 const int NUM_ELEM = 10;
 int main()
 {
-  // Alocação Estática
+  // Static Allocation
   int c[NUM_ELEM] = {1,2,3,4,5,6,7,8,9,10};
 
-  // Alocação Dinâmica
+  // Dinamic Allocation
   int *d = new int[NUM_ELEM];
   for (int i = 0; i < NUM_ELEM; i++){
     d[i] = i + 1;
   }
   
-  modifica_vetor_sintaxe_1(c, NUM_ELEM);
-  modifica_vetor_sintaxe_2(c, NUM_ELEM);
+  modify_vetor_sintax_1(c, NUM_ELEM);
+  modify_vetor_sintax_2(c, NUM_ELEM);
 
-  modifica_vetor_sintaxe_1(d, NUM_ELEM);
-  modifica_vetor_sintaxe_2(d, NUM_ELEM);
+  modify_vetor_sintax_1(d, NUM_ELEM);
+  modify_vetor_sintax_2(d, NUM_ELEM);
 
   for (int i = 0; i < NUM_ELEM; i++){
     std::cout << i << " : " << c[i] << " , " << d[i] << std::endl;
