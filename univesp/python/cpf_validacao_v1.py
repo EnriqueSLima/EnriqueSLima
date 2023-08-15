998d21e34d68ab8fcb989b534ef1ad93f45c6a94
EnriqueSLima <souza.enrique7@gmail.com>
2023-08-15 14:13:46 -0300

    Creating 2 versions with conditionals and with functions

--- univesp/python/cpf_validacao_v1.py
+++ univesp/python/cpf_validacao_v1.py
@@ -21,6 +21,8 @@
 
 """ 
 
+# Este código possui diversas formas de ser feito, nessa versão não usaremos funções
+
 entrada_cpf = input('Digite o CPF:')        #   Pergunta ao usuário o número do CPF e atribui a variável 'entrada_cpf'.
 remove_char = ['.', ',', ' ', '-']          #   Define possíveis caracteres no CPF que não desejamos
 
@@ -29,25 +31,25 @@ numerico_cpf = entrada_cpf.translate(str.maketrans({ord(x): '' for x in remove_c
 
 print('CPF sem pontuação: ', numerico_cpf)          #   Imprime CPF sem pontuação para verificação (OPCIONAL)
 
-if len(numerico_cpf) == 11:         #   Verifica se o CPF sem a pontuação possui 11 caracteres  
+if len(numerico_cpf) != 11:                 #   Verifica se o CPF sem a pontuação possui 11 caracteres
+        print('CPF deve conter 11 digitos.')             #   Mensagem de erro caso não possua 11 caracteres
+else:
     somador_etapa1 = 0              #   Criando uma variável inteira para representar a soma dos indices multiplicados
 
     #   Criando looping de 'for' para iteração de multiplicação entre os índices
     for i in range(9):                                                  #   Iteração até o elemento 9
-        multiplicador_etapa1 = int(numerico_cpf[i]) * (int(i)+1)        #   Transforma o valor 'i' em inteiro e multiplica por ele mesmo
+        multiplicador_etapa1 = int(numerico_cpf[i]) * (int(i)+1)        #   Transforma o valor 'i' em inteiro e multiplica por 'i + 1'
         somador_etapa1 = multiplicador_etapa1 + somador_etapa1          #   Atribui o valor da multiplicação de forma recursiva na variável 'somando_passo2' 
 
-    print('Resultado da primeira soma: ', somador_etapa1)               #   Imprime o valor da 'somador_etapa1' (OPCIONAL)
+    #print('Resultado da primeira soma: ', somador_etapa1)               #   Imprime o valor da 'somador_etapa1' (OPCIONAL)
 
-    resto_etapa1 = somador_etapa1 % 11                          #   Dividindo o valor de 'somador_etapa1' por 11 e atribuindo ao resto
-    print('Resto da primeira divisão: ', resto_etapa1)          #   Imprime o valor da 'resto_etapa1' (OPCIONAL)
-    if resto_etapa1 >= 10:
+    resto_etapa1 = somador_etapa1 % 11                          #   Dividindo o valor de 'somador_etapa1' por 11 e atribuindo ao 'resto_etapa1'
+    if resto_etapa1 >= 10:                                      #   Verifica se o 'resto_etapa1' é maior ou igual a 10
         resto_etapa1 = resto_etapa1%10
-        print('Caso >= 10: ', resto_etapa1)
-    else:
-        resto_etapa1 = resto_etapa1
+    
+    print('Resto da primeira divisão: ', resto_etapa1)          #   Imprime o valor da 'resto_etapa1' (OPCIONAL)
 
-    if resto_etapa1 == int(numerico_cpf[-2]):                   #   Verificando o digito verificador
+    if resto_etapa1 == int(numerico_cpf[-2]):                   #   Verificando o primeiro digito verificador
 
         somador_etapa2 = 0              #   Criando uma variável inteira para representar a soma dos indices multiplicados
 
@@ -56,14 +58,16 @@ if len(numerico_cpf) == 11:         #   Verifica se o CPF sem a pontuação poss
             multiplicador_etapa2= int(numerico_cpf[i]) * int(i)             #   Transforma o valor 'i' em inteiro e multiplica por ele mesmo
             somador_etapa2 = multiplicador_etapa2 + somador_etapa2          #   Atribui o valor da multiplicação de forma recursiva na variável 'somando_passo2' 
 
-        print('Resultado da segunda soma: ', somador_etapa2)                #   Imprime o valor da 'somador_etapa2' (OPCIONAL)
+        #print('Resultado da segunda soma: ', somador_etapa2)                #   Imprime o valor da 'somador_etapa2' (OPCIONAL)
 
-        resto_etapa2= somador_etapa2 % 11                           #   Dividindo o valor de 'somando' por 11 e atribuindo ao resto
-        print('Resto da segunda divisão: ', resto_etapa2)           #   Imprime o valor da 'resto_etapa1' (OPCIONAL)
-        if resto_etapa2 >= 10:
+        resto_etapa2= somador_etapa2 % 11                           #   Dividindo o valor de 'somando' por 11 e atribuindo ao 'resto_etapa2'
+
+        if resto_etapa2 >= 10:                                      # Verifica se o resto é maior ou igual a 10
             resto_etapa2 = resto_etapa2%10
+        
+        print('Resto da segunda divisão: ', resto_etapa2)          #   Imprime o valor da 'resto_etapa2' (OPCIONAL)
 
-        if resto_etapa2 == int(numerico_cpf[-1]):                   #   Verificando o digito verificador
-            print('CPF válido.')
-else:
-    print('CPF inválido.')
+        if resto_etapa2 == int(numerico_cpf[-1]):                   #   Verificando o segundo digito verificador
+            print('CPF válido')
+    else:                                                           #   Caso não ocorra verificação correta dos digitos
+        print('CPF inválido')
